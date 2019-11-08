7d779735cdb92816f291006ea04c91c44246a188
itallo gravina <itallo.gravina@gmail.com>
2019-11-08 13:05:31 -0300

    #1 Configurando rotas e arumando pages

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import Main from "./pages/Main";
+import Manutencao from "./pages/Manutencao";
+import Instalacao from "./pages/Instalacao";
+
+
+const Routes = () =>(
+<BrowserRouter>
+ <Switch>
+   <Route exact path="/" component={Main}/>
+   <Route path="/consultoria" component={()=> <h1>consultoria </h1>}/>
+   <Route path="/manutencao" component={Manutencao}/>
+   <Route path="/instalacao" component={Instalacao}/>
+ </Switch>
+</BrowserRouter>
+);
+
+export default Routes;
