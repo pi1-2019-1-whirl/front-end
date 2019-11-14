bbc681f19667918db2d9ae9053dc17b0284447ae
itallo gravina <itallo.gravina@gmail.com>
2019-11-14 16:04:25 -0300

    Página de Manutenção #5

--- src/pages/Manutencao.js
+++ src/pages/Manutencao.js
@@ -1,11 +1,54 @@
-import React from "react";
+import React, {Component} from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { Line,Bar,Pie } from 'react-chartjs-2';
+import CanvasJSReact from "../components/assets/canvasjs.react";
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-const Manutencao = () => (<div className="App">
-<Navbar />
-<main className="Main" style = {{marginTop: '64px'}}> 
-<p>This is the page content !</p>
-</main>
-</div>);
 
+class Manutencao extends Component{
+ render() {
+  
+		const options = {
+
+			title: {
+				text: "Energia Gerada"
+			},		axisY:[{
+    title: "Voltagem",       
+   }],
+   axisX:[{
+    title: "Tempo",       
+   }],
+  
+			animationEnabled: false,
+			data: [
+			{
+				// Change type to "doughnut", "line", "splineArea", etc.
+				type: "column",
+				dataPoints: [
+					{ label: "Apple",  y: 10876  },
+					{ label: "Orange", y: 15123 },
+					{ label: "Banana", y: 23123  },
+					{ label: "Mango",  y: 30222 },
+					{ label: "Orange", y: 15224  },
+					{ label: "Banana", y: 25454  },
+
+				]
+			}
+			]
+		}
+		
+		return (
+  <div>
+   <Navbar/>
+		<div>
+			<h1>React Column Chart</h1>
+			<CanvasJSChart options = {options} 
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+  </div>
+		);
+	}
+}
 export default Manutencao;
