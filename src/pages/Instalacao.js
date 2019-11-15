f813852449e5b5cd54ec96d65bcdcacf79591996
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-15 13:16:46 -0300

    Mudando as fontes do form

--- src/pages/Instalacao.js
+++ src/pages/Instalacao.js
@@ -1,24 +1,35 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import "../components/css/Instalacao.css"
 
 const Instalacao = () => (<div className="App">
 <Navbar />
 <main className="Main" style = {{marginTop: '64px'}}> 
-    <h1>Instalação</h1>
-    <form>
-        <label>
-            Altura da caixa d'água (m):
+<link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
+    <body>
+    <h1 class="titulo_instalacao">Instalação</h1>
+    <form class="formulario">
+        <label class="altura_caixa">
+            <a>Altura da caixa d'água (m):</a>
+            <p></p>
             <input type="number" step="0.01" name="altura_caixa"/>
-        </label>
-        <label>
+        </label >
+        <p></p>
+        <label class="capacidade_caixa">
             Capacidade da caixa d'água (l):
-            <input type="number" step="0.01" name="altura_caixa"/>
+            <p></p>
+            <input type="number" step="0.01" name="capacidade_caixa"/>
         </label>
-        <label>
+        <p></p>
+        <label class="tubulacao_caixa">
             Diâmetro da tubulação conectada á caixa (cm):
-            <input type="number" step="0.01" name="altura_caixa"/>
+            <p></p>
+            <input type="number" step="0.01" name="tubulacao_caixa"/>
         </label>
+        <p></p>
+        <button>Submeter</button>
     </form>
+    </body>
 </main>
 </div>);
 
