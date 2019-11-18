1daae2745797f715e75a4e079ff65cffd8bc3617
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-18 17:19:09 -0300

    Refatorando o código e salvando as variáveis

--- src/pages/Instalacao.js
+++ src/pages/Instalacao.js
@@ -1,41 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import "../components/css/Instalacao.css"
 
-const Instalacao = () => (<div className="App">
-<Navbar />
-<main className="Main" style = {{marginTop: '64px'}}> 
-<link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
-    <body>
-    <h1 class="titulo_instalacao">Instalação</h1>
-    <form class="formulario">
-        <label class="altura_caixa">
-            Altura da caixa d'água (m):
-            <p></p>
-            <input type="number" step="0.01" name="altura_caixa"/>
-        </label >
-        <p></p>
-        <label class="capacidade_caixa">
-            Capacidade da caixa d'água (l):
-            <p></p>
-            <input type="number" step="0.01" name="capacidade_caixa"/>
-        </label>
-        <p></p>
-        <label class="tubulacao_caixa">
-            Diâmetro da tubulação conectada á caixa (cm):
-            <p></p>
-            <input type="number" step="0.01" name="tubulacao_caixa"/>
-        </label>
-        <p></p>
-        <button>Submeter</button>
-    </form>
-    <div class="resultado">
-        <h1>
-            Energia gerada
-        </h1>
-    </div>
-    </body>
-</main>
-</div>);
+class Instalacao extends Component {
+    state = {
+        altura_caixa: null,
+        capacidade_caixa: null,
+        tubulacao_caixa: null,
+    }
+
+    handleInput = ({target}) => {
+        this.setState({ [target.name]: target.value })
+        console.log(this.state.altura_caixa)
+        console.log(this.state.capacidade_caixa)
+        console.log(this.state.tubulacao_caixa)
+    }
+
+    render() {
+        return (
+            <div className="App">
+            <Navbar />
+            <main className="Main"> 
+            <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
+                <body>
+                <h1 className="titulo_instalacao">Instalação</h1>
+                <form className="formulario">
+                    <label className="altura_caixa">
+                        Altura da caixa d'água (m):
+                        <p></p>
+                        <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa"/>
+                    </label >
+                    <p></p>
+                    <label className="capacidade_caixa">
+                        Capacidade da caixa d'água (l):
+                        <p></p>
+                        <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa"/>
+                    </label>
+                    <p></p>
+                    <label className="tubulacao_caixa">
+                        Diâmetro da tubulação conectada á caixa (cm):
+                        <p></p>
+                        <input required type="number" step="0.01" value={this.state.tubulacao_caixa} onChange={this.handleInput} name="tubulacao_caixa"/>
+                    </label>
+                    <p></p>
+                    <button>Submeter</button>
+                </form>
+                <div className="resultado">
+                    <h1>
+                        Energia gerada
+                    </h1>
+                </div>
+                </body>
+            </main>
+            </div>
+        )
+    }
+};
 
 export default Instalacao;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,15 +4,12 @@ import logo from "../components/imagens/logo2.png"
 
 const Main = () => (
 <div className="App">
-      <main className="Main"> 
       <img className="Logo" src={logo} alt="Logo" />
       <h1 className="Nome">WHIRL</h1>
       <div className="button-main">
-      <a className ="butinst" href="/instalacao"> Instalação</a>{" "}
-      <a className ="butmanu" href="/manutencao">Manutenção </a>   
- 
+            <a className ="butinst" href="/instalacao"> Instalação</a>{" "}
+            <a className ="butmanu" href="/manutencao">Manutenção </a>   
       </div>
-      </main>
 </div>
 );
 
