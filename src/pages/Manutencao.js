43bac58500bb59c27a7f9f8ce7d35471200b3974
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-19 21:32:27 -0300

    Modelando o gráfico

--- src/pages/Manutencao.js
+++ src/pages/Manutencao.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import { Line, Bar, Pie } from 'react-chartjs-2';
+import "../components/css/Manutencao.css";
 import CanvasJSReact from "../components/assets/canvasjs.react";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-var dps = [{ x: 1, y: 10 }, { x: 2, y: 13 }, { x: 3, y: 18 }, { x: 4, y: 20 }, { x: 5, y: 17 }, { x: 6, y: 10 }, { x: 7, y: 13 }, { x: 8, y: 18 }, { x: 9, y: 20 }, { x: 10, y: 17 }];   //dataPoints.
+var dps = [{ x: 1, y: 10 }, { x: 2, y: 13 }, { x: 3, y: 18 }];   //dataPoints.
 var xVal = dps.length + 1;
 var yVal = 15;
 var updateInterval = 1000;
@@ -13,9 +14,11 @@ class Manutencao extends Component {
 		super();
 		this.updateChart = this.updateChart.bind(this);
 	}
+
 	componentDidMount() {
 		setInterval(this.updateChart, updateInterval);
 	}
+
 	updateChart() {
 		yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
 		dps.push({ x: xVal, y: yVal });
@@ -25,24 +28,29 @@ class Manutencao extends Component {
 		}
 		this.chart.render();
 	}
+
 	render() {
 		const options = {
+			animationEnabled: true,
 			title: {
 				text: "Energia Gerada"
 			},
 			data: [{
-				type: "line",
-				dataPoints: dps
+				
+				type: "spline",
+				lineThickness: 4,
+				lineColor: "green",
+				dataPoints: dps, color: "dark-green"
 			}]
 		}
 
 		return (
-			<div>
-				<Navbar />
-				<div>
+			<div className="App">
+            <Navbar />
+				<div className="Gráfico">
 					<h1>React Column Chart</h1>
 					<CanvasJSChart options={options}
-					onRef={ref => this.chart = ref}
+						onRef={ref => this.chart = ref}
 					/>
 					{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
 				</div>
