25f017b4f8cf9ea97b150cfeb766483999785c36
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-20 00:07:44 -0300

    Salvar os requisitos do banco de dados

--- src/pages/Manutencao.js
+++ src/pages/Manutencao.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import axios from 'axios';
 import Navbar from "../components/Navbar/Navbar";
 import { Line, Bar, Pie } from 'react-chartjs-2';
 import "../components/css/Manutencao.css";
 import CanvasJSReact from "../components/assets/canvasjs.react";
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-var dps = [{ x: 1, y: 10 }, { x: 2, y: 13 }, { x: 3, y: 18 }];   //dataPoints.
+var dps = [{ x: 1, y: 10 }, { x: 2, y: 13 }];   //dataPoints.
 var xVal = dps.length + 1;
 var yVal = 15;
 var updateInterval = 1000;
@@ -15,12 +16,28 @@ class Manutencao extends Component {
 		this.updateChart = this.updateChart.bind(this);
 	}
 
+	state = {
+		power: 0,
+		current: 0,
+		is_working: true,
+	}
+
 	componentDidMount() {
 		setInterval(this.updateChart, updateInterval);
 	}
 
 	updateChart() {
-		yVal = yVal + Math.round(5 + Math.random() * (-5 - 5));
+		axios.get(``)  /*LINK PARA O SITE */
+			.then(res => {
+				const response = res.data;
+				this.state.power = response.power;
+				this.state.current = response.current;
+				this.state.is_working = response.is_working;
+				//console.log(this.state.power)
+				//console.log(this.state.current)
+				//console.log(this.state.is_working)
+			})
+		yVal = this.state.current;
 		dps.push({ x: xVal, y: yVal });
 		xVal++;
 		if (dps.length > 10) {
@@ -36,7 +53,7 @@ class Manutencao extends Component {
 				text: "Energia Gerada"
 			},
 			data: [{
-				
+
 				type: "spline",
 				lineThickness: 4,
 				lineColor: "green",
@@ -46,13 +63,20 @@ class Manutencao extends Component {
 
 		return (
 			<div className="App">
-            <Navbar />
+				<Navbar />
 				<div className="Gráfico">
 					<h1>React Column Chart</h1>
 					<CanvasJSChart options={options}
 						onRef={ref => this.chart = ref}
 					/>
-					{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+				</div>
+				<div>
+					<h1>
+						Está funcionando ?
+						{<h1>{(this.state.is_working) ? "SIM" : "NÃO" }</h1>}
+					</h1>
+					<h1>
+					</h1>
 				</div>
 			</div>
 		);
