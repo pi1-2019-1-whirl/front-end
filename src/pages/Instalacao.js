3c5a5d4fe0fed319d63cd700c5373f3258ab1070
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-27 21:07:08 -0300

    Condições para as opções

--- src/pages/Instalacao.js
+++ src/pages/Instalacao.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import "../components/css/Instalacao.css"
 
-
 class Instalacao extends Component {
+
+
     state = {
         altura_caixa: 0,
         capacidade_caixa: 0,
         tubulacao_caixa: 0,
         verifica_posicao: true,
+        vazao_bomba: 0,
     }
 
     handleInput = ({ target }) => {
         this.setState({ [target.name]: target.value })
+        
         console.log(this.state.altura_caixa)
         console.log(this.state.capacidade_caixa)
         console.log(this.state.tubulacao_caixa)
@@ -30,39 +33,69 @@ class Instalacao extends Component {
                     <div>
                         <h1 className="titulo_instalacao">Instalação</h1>
                         <br></br>
-                        <form className="formulario">
-                            <label>Em que parte está localizada a sua caixa</label>
-                            <div>
-                                <div className="radio-item">
-                                    <input defaultChecked={this.state.verifica_posicao} type="radio" id="ritema" name="verifica_posicao" onClick={this.handleInput} value="true" />
-                                    <label htmlFor="ritema">Superior</label>
-                                </div>
-                                <div className="radio-item">
-                                    <input type="radio" id="ritemb" name="verifica_posicao" onClick={this.handleInput} value="false"/>
-                                    <label htmlFor="ritemb">inferior</label>
-                                </div>
-                            </div>
-                            <br></br>
-                            <label className="altura_caixa">
-                                Altura da caixa d'água (m):
-                        <p></p>
-                                <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
-                            </label >
-                            <p></p>
-                            <label className="capacidade_caixa">
-                                Capacidade da caixa d'água (l):
-                        <p></p>
-                                <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
-                            </label>
-                            <p></p>
-                            <label className="tubulacao_caixa">
-                                Diâmetro da tubulação conectada á caixa (cm):
-                        <p></p>
-                                <input required type="number" step="0.01" value={this.state.tubulacao_caixa} onChange={this.handleInput} name="tubulacao_caixa" />
-                            </label>
-                            <p></p>
-                            <button>Submeter</button>
-                        </form>
+                        <div className="posicao_caixa">
+
+                        <label>Em que parte está localizada a sua caixa d'água ?</label>
+                                    <div>
+                                        <div className="radio-item">
+                                            <input defaultChecked={this.state.verifica_posicao} type="radio" id="ritema" name="verifica_posicao" onClick={this.handleInput} value={true} />
+                                            <label htmlFor="ritema">Superior</label>
+                                        </div>
+                                        <div className="radio-item">
+                                            <input type="radio" id="ritemb" name="verifica_posicao" onClick={this.handleInput} value={false} />
+                                            <label htmlFor="ritemb">inferior</label>
+                                        </div>
+                                    </div>
+                        </div>
+                        <div>
+                            {this.state.verifica_posicao ? (
+                                console.log("em cima"),
+                                <form className="formulario">
+                                    <br></br>
+                                    <label className="altura_caixa">
+                                        Altura da caixa d'água (m):
+                                <p></p>
+                                        <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                    </label >
+                                    <p></p>
+                                    <label className="capacidade_caixa">
+                                        Capacidade da caixa d'água (l):
+                                <p></p>
+                                        <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                    </label>
+                                    <p></p>
+                                    <p></p>
+                                    <button>Submeter</button>
+                                </form>
+                            ) : (
+                                console.log("embaixo"),
+                                <form className="formulario">
+                                    <br></br>
+                                    <label className="altura_caixa">
+                                        Altura da caixa d'água (m):
+                                <p></p>
+                                        <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                    </label >
+                                    <p></p>
+                                    <label className="capacidade_caixa">
+                                        Capacidade da caixa d'água (l):
+                                <p></p>
+                                        <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                    </label>
+                                    <p></p>
+                                    <label className="vazao_bomba">
+                                        Vazão da bomba:
+                                <p></p>
+                                        <input required type="number" step="0.01" value={this.state.vazao_bomba} onChange={this.handleInput} name="vazao_bomba" />
+                                    </label>
+                                    <p></p>
+                                    <p></p>
+                                    <button>Submeter</button>
+                                </form>
+                                    )}
+                        </div>
+
+
                         <br></br>
                         <br></br>
                         <br></br>
