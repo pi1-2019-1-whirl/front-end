285343fc3dd5cea0321f62b3f32a5e8835846b2b
Ítalo Vinícius <italovinicius18@yahoo.com>
2019-11-30 11:27:16 -0300

    Arrumando o input e mundando o cálculo da energia e pontência usual gerada

--- src/pages/Instalacao.js
+++ src/pages/Instalacao.js
@@ -10,16 +10,12 @@ class Instalacao extends Component {
         capacidade_caixa: 0,
         tubulacao_caixa: 0,
         verifica_posicao: 1,
-        vazao_bomba: 0,
+        vazao_bomba: 0.2,
     }
 
     handleInput = ({ target }) => {
-        if(target.name === 'verifica_posicao')
-            this.setState({ [target.name]: parseFloat(target.value) })
-        else 
-            this.setState({ [target.name]: target.value })
+        this.setState({ [target.name]: parseFloat(target.value) })
     }
-
 
     render() {
 
@@ -29,6 +25,8 @@ class Instalacao extends Component {
                 <div className="Main">
                     <link href="https://fonts.googleapis.com/css?family=Varela+Round&display=swap" rel="stylesheet"></link>
                     <div>
+                        <br></br>
+                        <br></br>
                         <h1 className="titulo_instalacao">Instalação</h1>
                         <br></br>
                         <div className="posicao_caixa">
@@ -52,17 +50,23 @@ class Instalacao extends Component {
                                     <label className="altura_caixa">
                                         Altura da caixa d'água (m):
                                 <p></p>
-                                        <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                        {this.state.altura_caixa ? (
+                                            <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                        ) : (
+                                                <input required type="number" step="0.01" value="null" onChange={this.handleInput} name="altura_caixa" />
+                                            )}
                                     </label >
                                     <p></p>
                                     <label className="capacidade_caixa">
                                         Capacidade da caixa d'água (l):
                                 <p></p>
-                                        <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                        {this.state.capacidade_caixa ? (
+                                            <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                        ) : (
+                                                <input required type="number" step="0.01" value="null" onChange={this.handleInput} name="capacidade_caixa" />
+                                            )}
                                     </label>
                                     <p></p>
-                                    <p></p>
-                                    <button>Submeter</button>
                                 </form>
                             ) : (
                                     <form className="formulario">
@@ -70,23 +74,33 @@ class Instalacao extends Component {
                                         <label className="altura_caixa">
                                             Altura da caixa d'água (m):
                                 <p></p>
-                                            <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                            {this.state.altura_caixa ? (
+                                                <input required type="number" step="0.01" value={this.state.altura_caixa} onChange={this.handleInput} name="altura_caixa" />
+                                            ) : (
+                                                    <input required type="number" step="0.01" value="null" onChange={this.handleInput} name="altura_caixa" />
+                                                )}
                                         </label >
                                         <p></p>
                                         <label className="capacidade_caixa">
                                             Capacidade da caixa d'água (l):
                                 <p></p>
-                                            <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                            {this.state.capacidade_caixa ? (
+                                                <input required type="number" step="0.01" value={this.state.capacidade_caixa} onChange={this.handleInput} name="capacidade_caixa" />
+                                            ) : (
+                                                    <input required type="number" step="0.01" value="null" onChange={this.handleInput} name="capacidade_caixa" />
+                                                )}
                                         </label>
                                         <p></p>
                                         <label className="vazao_bomba">
                                             Vazão da bomba:
                                 <p></p>
-                                            <input required type="number" step="0.01" value={this.state.vazao_bomba} onChange={this.handleInput} name="vazao_bomba" />
+                                            {this.state.vazao_bomba ? (
+                                                <input required type="number" step="0.01" value={this.state.vazao_bomba} onChange={this.handleInput} name="vazao_bomba" />
+                                            ) : (
+                                                    <input required type="number" step="0.01" value="null" onChange={this.handleInput} name="vazao_bomba" />
+                                                )}
                                         </label>
                                         <p></p>
-                                        <p></p>
-                                        <button>Submeter</button>
                                     </form>
                                 )}
                         </div>
@@ -99,14 +113,14 @@ class Instalacao extends Component {
                             <div className="valor_calculado">
                                 <h1 >
                                     Energia gerada
-                                    <br></br> 
-                                    {this.state.vazao_bomba*this.state.altura_caixa*9.81+" KW"}
+                                    <br></br>
+                                    {parseFloat(this.state.vazao_bomba * this.state.altura_caixa * 9.81*(this.state.capacidade_caixa/this.state.vazao_bomba)).toFixed(2) + " KW"}
                                 </h1>
                                 <br></br>
                                 <h1 >
                                     Potência útil
-                                    <br></br> 
-                                    {this.state.altura_caixa*9.81*0.8+" KW"}
+                                    <br></br>
+                                    {parseFloat(this.state.altura_caixa * 9.81 * 0.8*(this.state.capacidade_caixa/this.state.vazao_bomba)).toFixed(2) + " KW"}
                                 </h1>
                             </div>
                         </div>
